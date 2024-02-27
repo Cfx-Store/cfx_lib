a0874461fbcc52f61da7d78ab5de409fefcd1477
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-02-27 12:04:17 +0100

    feat: addCommand

--- types.lua
+++ types.lua
@@ -22,3 +22,17 @@
 ---@alias AccountType
 ---| '"bank"'
 ---| '"money"'
+
+
+---@class CommandParams
+---@field name string
+---@field help? string
+---@field type? 'number' | 'playerId' | 'string'
+---@field optional? boolean
+---@field full? boolean
+
+
+---@class CommandProperties
+---@field help string?
+---@field params CommandParams[]?
+---@field restricted boolean | string | string[]?
